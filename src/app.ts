87c2f96308a2afc21d7370570cb79b3457d56c49
Nithish-kumar-asign <nithish.kumar@asign.art>
2026-01-07 22:36:33 +0530

    test webhook

--- src/app.ts
+++ src/app.ts
@@ -34,6 +34,7 @@ const swaggerOptions = {
 
 
 
+
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/auth', authRoutes);
